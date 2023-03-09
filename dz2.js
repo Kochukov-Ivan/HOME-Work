2e954c99e07a8b9ea247062541130dbe11847ad9
Kochukov-Ivan <125874758+Kochukov-Ivan@users.noreply.github.com>
2023-03-09 15:56:24 +0300

    Add files via upload

---
+++ dz2.js
@@ -0,0 +1,33 @@
+console.log('20.01.1997')
+console.log('Добро' + ' Пожаловать' + ' на курс ' + 'Иван')
+// alert('08.03.2023')
+document.getElementById("one").innerHTML = '<h2>Hello world</h2>'
+//Примитивные типы 
+//string (строка)
+//boolean (ложь правда )
+//number(число)
+//null 
+//undefined
+//symbol(символ)
+const a = null //null
+console.log(a)
+console.log(1 / 0); //infinity 
+let B = 'Ivan Kochukov'
+console.log(B)//string
+let numB2 = 99999999999999999999999n;
+console.log(numB2);
+let Ban = true
+let unBan = false
+console.log(Ban)
+console.log(unBan)
+let z;
+console.log(z)
+// typeof x
+// typeof (x)
+console.log(typeof undefined);
+console.log(typeof 2);
+console.log(typeof "cat");
+console.log(typeof 110n);
+console.log(typeof true);
+console.log(typeof Symbol("id"));
+console.log(typeof null);
